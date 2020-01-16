0e31ca6fe1da1bb708daa04afcfe759566fac84f
Filip Mularski <mularskif@gmail.com>
2020-01-16 21:47:17 +0100

    add controls for log in screen

---
+++ AccountManager/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls )
+            {
+                if ( control.GetType() == typeof(MdiClient) ||
+                    control.GetType() == typeof(Label))
+                {
+                    control.BackColor = Color.LightYellow;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
